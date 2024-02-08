de3c340f3bef08dba5264546e6bafc0ab1382331
vekim91 <mikeangelovelasco@gmail.com>
2024-02-07 19:56:31 -0500

    Final changes for submission

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# webdev_proj
+# Mike's Web Dev Final Project
+
+Welcome to my final project for the Web Dev Sprint! This repository contains the source code for a simple web app that has interative figures and checkboxes that act as data filters.
+
+## Getting Started
+
+To run this web app locally, follow these steps:
+1. Setup a virtual environment (OPTIONAL)
+```bash
+python -m venv venv
+.\venv\Scripts\activate
+```
+2. Install dependencies
+```bash
+pip install -r requirements.txt
+```
+3. Install Streamlit
+```bash
+pip install streamlit
+```
+
+
+### Prerequisites
+
+- Make sure you have [Python](https://www.python.org/) installed on your machine.
+- Download the following libraries:
+    - Plotly - ``` pip install plotly ```
+    - Pandas - ```pip install pandas```
+    - Scipy - ```pip install scipy```
+    - Numpy - ```pip install numpy```
+
+### Clone the Repository
+
+```bash
+git clone https://github.com/vekim91/webdev_proj.git
+```
+
+### Run the App
+
+```bash
+streamlit run app.py
+```
+
+### Open on Browser
+
+Use this link: http://127.0.0.1:10000/
--- app.py
+++ app.py
@@ -2,29 +2,32 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
+# Load the data
 try:
     data = pd.read_csv('vehicles_us.csv')
 except:
     data = pd.read_csv('https://raw.githubusercontent.com/vekim91/webdev_proj/main/vehicles_us.csv')
 
+# Fix the data to separate the make and the model
 data[['make','model']] = data['model'].str.split(' ',n=1,expand=True)
-data = data[['price', 'model_year', 'make', 'model', 'condition', 'cylinders', 'fuel', 'odometer', 'transmission', 'type', 'paint_color', 'is_4wd', 'date_posted', 'days_listed']]
 
+
+# Header copied from the notebook
 st.header('Introduction')
 st.write('We\'re starting this project by doing some EDA on the `vehicles_us.csv` file. The goal of the EDA is to generate some figures that would help us appreciate and digest the data easier. We do this by looking at our data in EDA, and see how we generate meaningful and interactive figures that we can export to Render using `Streamlit`.\n\n Let\'s load our data and see what would be interesting visuals to help us understand it better!')
 
 # Get the list of unique brands in the data
+st.write('***Select the make of the car that you want to see the data for in the figures below:***')
 brand_list = sorted(data['make'].unique())
+
+# Create columns for aesthetics of the checkboxes
+num_columns = 4
+cb_columns = st.columns(num_columns)
 
 # Initiate filter list
 filter_list = []
 
-num_columns = 4
-
-# Create columns for aesthetics
-cb_columns = st.columns(num_columns)
-
-# Iterate the unique list to generate checkboxes
+# Iterate the unique list to generate checkboxes and place them in the columns
 for i, brand in enumerate(brand_list):
     checkbox_value = cb_columns[i % num_columns].checkbox(label=brand)
 
@@ -36,6 +39,7 @@ for i, brand in enumerate(brand_list):
         except:
             pass
 
+# Create the interactive figures for the app
 scatter_fig = px.scatter(data_frame=data[data['make'].isin(filter_list)],
                         x='model_year',
                         y='price',
@@ -48,3 +52,10 @@ histo_fig = px.histogram(data_frame=data[data['make'].isin(filter_list)],
                         x='model_year',
                         title='Distribution of Cars per Year')
 st.plotly_chart(histo_fig)
+
+st.write(f'**Correlation Matrix for:** {filter_list}')
+data_corr_matrix = data[data['make'].isin(filter_list)].drop('is_4wd', axis=1)
+corr_matrix = data_corr_matrix.corr(numeric_only=True)
+st.dataframe(corr_matrix)
+
+st.write('I hope this project gave you a good impression of what I\'ve learned so far in this bootcamp, and I hope you enjoyed interacting with this simple web app!')
